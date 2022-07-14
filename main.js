7a964861df9518c55a587a2af858f93ced351e59
Granatov <gena2209@yandex.ru>
2022-07-14 18:56:50 +0500

    add hide a pass

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+"use strict";
+
 let brg = document.querySelector(".menu__brg");
 let menuList = document.querySelector(".menu__list");
 let loginLinks = document.querySelector(".login__info");
@@ -6,5 +8,16 @@ brg.addEventListener("click", function (event) {
   brg.classList.toggle("active");
   loginLinks.classList.toggle("active");
   menuList.classList.toggle("active");
-
 });
+
+function show_hide_password(target) {
+  let input = document.getElementById("password_input");
+  if (input.getAttribute("type") == "password") {
+    target.classList.add("view");
+    input.setAttribute("type", "text");
+  } else {
+    target.classList.remove("view");
+    input.setAttribute("type", "password");
+  }
+  return false;
+}
