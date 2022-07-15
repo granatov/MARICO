c40e32d848cdd8246a7675edcf80356be8d2d80d
Granatov <gena2209@yandex.ru>
2022-07-15 07:29:21 +0500

    fix scroll
    when brg menu is active - scroll is off

--- main.js
+++ main.js
@@ -3,15 +3,16 @@
 let brg = document.querySelector(".menu__brg");
 let menuList = document.querySelector(".menu__list");
 let loginLinks = document.querySelector(".login__info");
-
+let body = document.querySelector("body");
 brg.addEventListener("click", function (event) {
   brg.classList.toggle("active");
   loginLinks.classList.toggle("active");
   menuList.classList.toggle("active");
+  body.classList.toggle("active");
 });
 
 function show_hide_password(target) {
-  let input = document.querySelector('.form__password')
+  let input = document.querySelector(".form__password");
   if (input.getAttribute("type") == "password") {
     target.classList.add("view");
     input.setAttribute("type", "text");
