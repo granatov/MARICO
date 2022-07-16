cceea7bd7ea200e46b93209cc24f09f1732e1ed3
Granatov <gena2209@yandex.ru>
2022-07-16 12:47:13 +0500

    add popUp windows in tarifs

--- main.js
+++ main.js
@@ -23,3 +23,26 @@ function show_hide_password(target) {
   }
   return false;
 }
+
+let btns = document.querySelectorAll("*[data-modal-btn]");
+
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    let name = btns[i].getAttribute("data-modal-btn");
+    let modal = document.querySelector("[data-modal-window = '" + name + "']");
+    modal.style.display = "block";
+    let close = modal.querySelector(".modal__close-window");
+    close.addEventListener("click", function () {
+      modal.style.display = "none";
+    });
+  });
+}
+
+window.onclick = function (event) {
+  if (event.target.hasAttribute("data-modal-window")) {
+    let modals = document.querySelectorAll("*[data-modal-window]");
+    for (let i = 0; i < modals.length; i++) {
+      modals[i].style.display = "none";
+    }
+  }
+};
